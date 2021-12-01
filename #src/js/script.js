1a09d314cfceca4ff251bc0509849056b4ab0fc9
Fedotos228 <tudor.onceanu@gmail.com>
2021-12-01 20:30:57 +0200

    home modal

--- #src/js/script.js
+++ #src/js/script.js
@@ -4,3 +4,20 @@ var swiper = new Swiper(".mainSwiper", {
         prevEl: ".swiper-button-prev",
     },
 });
+
+const serviceBtn = document.querySelectorAll('.services-btn')
+const serviceModal = document.querySelector('.services-details')
+
+if (serviceModal) {
+    serviceBtn.addEventListener("click", function(event) {
+        event.preventDefault();
+        serviceModal.classList.add('open');
+        document.body.classList.add('lock');
+    })
+    window.addEventListener("click", function(event) {
+        if (event.target == serviceModal) {
+            serviceModal.classList.remove("open");
+            document.body.classList.remove('lock');
+        }
+    })
+}
