f177bca93df3d87477496dd19ecef2e80c4756f4
Fedotos228 <tudor.onceanu@gmail.com>
2021-12-08 02:17:54 +0200

    offers

--- #src/js/script.js
+++ #src/js/script.js
@@ -8,16 +8,95 @@ var swiper = new Swiper(".mainSwiper", {
 const serviceBtn = document.querySelectorAll('.services-btn')
 const serviceModal = document.querySelector('.services-details')
 
+
+
 if (serviceModal) {
-    serviceBtn.addEventListener("click", function(event) {
-        event.preventDefault();
-        serviceModal.classList.add('open');
-        document.body.classList.add('lock');
+    serviceBtn.forEach(serviceBtn => {
+        serviceBtn.addEventListener("click", function(event) {
+            event.preventDefault();
+            serviceModal.classList.add('open');
+            document.body.classList.add('lock');
+        })
+        window.addEventListener("click", function(event) {
+            if (event.target == serviceModal) {
+                serviceModal.classList.remove("open");
+                document.body.classList.remove('lock');
+            }
+        })
     })
+
+}
+
+const counters = document.querySelectorAll('.statistics-counter');
+const statistics = document.querySelector('.statistics')
+const speed = 100;
+
+window.addEventListener('scroll', () => {
+    const statisticPos = statistics.offsetTop;
+    const scrollPos = window.scrollY;
+
+    if (scrollPos >= statisticPos - 300) {
+        counters.forEach(counter => {
+            const animate = () => {
+                const value = +counter.getAttribute('akhi');
+                const data = +counter.innerText;
+
+                const time = value / speed;
+                if (data < value) {
+                    counter.innerText = Math.ceil(data + time);
+                    setTimeout(animate, 1);
+                } else {
+                    counter.innerText = value;
+                }
+
+            }
+            animate();
+        });
+    }
+});
+
+const headerLeft = document.querySelector('.upper-header__left');
+const headerNav = document.querySelector('.header-nav');
+
+window.addEventListener('resize', () => {
+    adaptive_function();
+});
+
+function adaptive_header(w, h) {
+    var upperHeaderBody = document.querySelector('.upper-header__body');
+
+    var result = headerLeft.classList.contains('done');
+    if (w < 767) {
+        if (!result) {
+            headerLeft.classList.add('done');
+            headerNav.insertBefore(headerLeft, headerNav.lastChild);
+        }
+    } else {
+        if (result) {
+            headerLeft.classList.remove('done');
+            upperHeaderBody.insertBefore(headerLeft, upperHeaderBody.firstChild);
+        }
+    }
+}
+
+function adaptive_function() {
+    var w = window.innerWidth;
+    var h = window.innerHeight;
+    adaptive_header(w, h);
+}
+adaptive_function();
+
+const burger = document.querySelector('.burger')
+
+burger.addEventListener('click', () => {
+    burger.classList.toggle('active')
+    headerNav.classList.toggle('active')
+
+
     window.addEventListener("click", function(event) {
-        if (event.target == serviceModal) {
-            serviceModal.classList.remove("open");
-            document.body.classList.remove('lock');
+        if (event.target != burger || event.target != headerNav) {
+            burger.classList.remove("active")
+            headerNav.classList.remove("active");
         }
     })
-}
+})
